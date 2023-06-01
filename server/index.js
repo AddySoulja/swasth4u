721df2c3e4bfc82b12970441f9c9779a4131ecb4
Aditya Kumar <adityadhanraj1357@gmail.com>
2023-06-02 02:17:52 +0530

    dev

--- server/index.js
+++ server/index.js
@@ -19,14 +19,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(cookieParser());
 
+app.get("/", (req, res) => {
+  res.status(200).json({ ok: true, message: "working api..." });
+});
 app.use("/api/users", userRoutes);
 
 if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, "/client/public")));
+  app.use(express.static(path.join(__dirname, "/client/build/static")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "public", "index.html"))
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
