a57aecf9d0a9589ba3ecf2901c001e175a9ba5d7
Aditya Kumar <adityadhanraj1357@gmail.com>
2023-06-01 21:13:55 +0530

    deploy

--- server/index.js
+++ server/index.js
@@ -1,9 +1,8 @@
 import path from "path";
 import express from "express";
 import dotenv from "dotenv";
-if (process.env.NODE_ENV !== "production") {
-  dotenv.config({ path: __dirname + "/.env" });
-}
+import cors from "cors";
+dotenv.config();
 import cookieParser from "cookie-parser";
 import connectDatabase from "./database/index.js";
 import userRoutes from "./RootRouter/index.js";
@@ -17,17 +16,17 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(cors());
 app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
 
 if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, "../client", "build")));
+  app.use(express.static(path.join(__dirname, "/client/public")));
 
-  app.get("/*", (req, res) =>
-    res.sendFile(path.join(__dirname, "../client", "build", "index.html"))
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "public", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
